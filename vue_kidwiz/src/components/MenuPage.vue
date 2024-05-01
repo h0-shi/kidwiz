d0365cdf258b4ad2efdb297175340b7d1a774b5e
h0-shi <whdgus9655@naver.com>
2024-05-01 12:46:06 +0900

    vue insert test

--- vue_kidwiz/src/components/MenuPage.vue
+++ vue_kidwiz/src/components/MenuPage.vue
@@ -3,6 +3,7 @@
     <nav class="nav">
         <li><router-link to="/testVue">테스트뷰</router-link></li>
         <li><router-link to="/listTest">DB테스트</router-link></li>
+        <li><router-link to="/insertTest">insert 테스트</router-link></li>
     </nav>
   </header>
 </template>
--- vue_kidwiz/src/router/index.js
+++ vue_kidwiz/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import testVue from "@/views/TestVue.vue";
 import listTest from "@/views/ListTest.vue";
+import insertTest from "@/views/insertTest.vue";
 
 const routes = [
     {path: '/testVue', name:'testVue', component: testVue},
-    {path: '/listTest', name:'listTest', component: listTest}
+    {path: '/listTest', name:'listTest', component: listTest},
+    {path: '/insertTest', name:'insertTest', component: insertTest}
 ];
 
 const router = createRouter({
