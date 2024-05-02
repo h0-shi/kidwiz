57ddb0b92a3167629059d8c8c1cb2a65a8e42951
h0-shi <whdgus9655@naver.com>
2024-05-02 17:48:29 +0900

    새로고침 404 잡는중

--- vue_kidwiz/vue.config.js
+++ vue_kidwiz/vue.config.js
@@ -1,10 +1,14 @@
 module.exports = {
+  outputDir: "../src/main/resource/static",
+  indexPath: "../static/index.html",
   devServer: {
+    historyApiFallback: true,
     proxy: {
         '/':{
             "target":'http://localhost:3000', // Spring boot의 주소 및 포트
             "pathRewrite":{'^/':''},
             "changeOrigin":true,
+            ws: false
         }
     }
   }
