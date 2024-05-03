d0eb62edf8d73f80c577a6d7c0fce8610112e550
chorok5 <cr.greenapple@gmail.com>
2024-05-03 19:25:08 +0900

    test up

--- kidwiz/src/main/java/com/kidwiz/web/repository/CareerTestRepository.java
+++ kidwiz/src/main/java/com/kidwiz/web/repository/CareerTestRepository.java
@@ -1,5 +1,8 @@
 package com.kidwiz.web.repository;
 
-public interface CareerTestRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CareerTestRepository extends JpaRepository<T, Integer> {
 
 }
