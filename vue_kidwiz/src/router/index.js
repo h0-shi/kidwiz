fbd367edbb3423fcb3f8b0c87a4ce1677f1dd837
chorok5 <cr.greenapple@gmail.com>
2024-05-03 15:48:55 +0900

    test

--- vue_kidwiz/src/router/index.js
+++ vue_kidwiz/src/router/index.js
@@ -3,13 +3,15 @@ import testVue from "@/views/TestVue.vue";
 import listTest from "@/views/ListTest.vue";
 import Boardwrite from "@/views/BoardWrite.vue";
 import insertTest from "@/views/insertTest.vue";
+import PersonTest from "@/views/PersonTest.vue";
 
 const routes = [
     {path: '/testVue', name:'testVue', component: testVue},
     {path: '/listTest', name:'listTest', component: listTest},
     {path:'/BoardList', name:'BoardList', component: () => import('../views/BoardList.vue')},
     {path:'/BoardWrite', name:'BoardWrite', component: Boardwrite},
-    {path: '/insertTest', name:'insertTest', component: insertTest}
+    {path: '/insertTest', name:'insertTest', component: insertTest},
+    {path:'/personTest', name: 'PersonTest', component: PersonTest}
 ];
 
 const router = createRouter({
