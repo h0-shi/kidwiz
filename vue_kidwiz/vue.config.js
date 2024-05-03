2d5eb3ee22453aaff4d0e36db3a32931b6535ec1
unknown <leenaland@gmail.com>
2024-05-03 16:02:06 +0900

    다시 커밋 시도

--- vue_kidwiz/vue.config.js
+++ vue_kidwiz/vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
     proxy: {
         '/':{
             "target":'http://localhost:3000', // Spring boot의 주소 및 포트
+            "ws": true, //websocket 프록시 활성화 - 이문희
             "pathRewrite":{'^/':''},
             "changeOrigin":true,
         }
