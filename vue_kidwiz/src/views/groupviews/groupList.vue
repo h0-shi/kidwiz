ccc75104c7edf3a1e6ccd623950f24c4dd4e5c2c
tjsdn3823 <tjsdn3823@naver.com>
2024-05-07 18:17:25 +0900

    test

--- vue_kidwiz/src/views/groupviews/groupList.vue
+++ vue_kidwiz/src/views/groupviews/groupList.vue
@@ -4,6 +4,10 @@
       <!-- row-cols-1은 행ㅇ에 열 1개씩 배치, 
         row-cols-sm-2 : 가로화면 크기가 576px보다 크거나 같으면 행에 2개씩 배치
         row-cols-md-3 : 가로화면 크기가 768px보다 크거나 같으면 행에 3개씩 배치  -->
+        
+      <!-- row-cols-1은 행ㅇ에 열 1개씩 배치, 
+        row-cols-sm-2 : 가로화면 크기가 576px보다 크거나 같으면 행에 2개씩 배치
+        row-cols-md-3 : 가로화면 크기가 768px보다 크거나 같으면 행에 3개씩 배치  -->
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
         <div class="col">
           <div class="card">
