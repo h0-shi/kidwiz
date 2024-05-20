cc779c558a156f548b91aa7a70f9e9632597405d
tjsdn3823 <tjsdn3823@naver.com>
2024-05-20 09:20:30 +0900

    새로고침

--- vue_kidwiz/src/components/MenuPage.vue
+++ vue_kidwiz/src/components/MenuPage.vue
@@ -20,17 +20,7 @@
           <a href="">회원가입</a>
         </span>
       </nav>
-
-      <nav class="nav-bot">
-        <div class="container">
-          <nav class="container d-flex flex-column flex-md-row justify-content-between">
-            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link>
-            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link>
-            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link>
-            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link>
-          </nav>
-        </div>
-        <!-- <ul>
+              <!-- <ul>
           <li><router-link to="/listTest">공지사항</router-link></li>
           <li><router-link to="/BoardList">상담</router-link></li>
           <li><router-link to="/insertTest">상담 정보</router-link></li>
@@ -58,63 +48,39 @@
           <li><router-link to="/contact">Contact</router-link></li>
           <li><router-link to="/admin">관리자페이지</router-link></li>
         </ul> -->
+      <nav class="nav-bot">
+        <div class="container">
+          <nav class="container d-flex flex-column flex-md-row justify-content-between">
+            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">센터 소개</router-link>
+            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담실</router-link>
+            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">소통/참여</router-link>
+            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">미디어콘텐츠</router-link>
+          </nav>
+        </div>
       </nav>
       <nav class="nav-menu">
-        <div class="parentsDiv container d-flex flex-column flex-md-row justify-content-between">
+        <div class="container">
           <nav class="container d-flex flex-column flex-md-row justify-content-between">
-            <!-- <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link>
-            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link>
-            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link>
-            <router-link class="py-2 d-none d-md-inline-block flex-fill" to="/BoardList">상담</router-link> -->
             <ul class="divUl py-2 d-none d-md-inline-block flex-fill">
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
+              <li><router-link to="listTest">공지사항</router-link></li>
+              <li><router-link to="/">이용안내</router-link></li>
+              <li><router-link to="faq">자주하는 질문<br>(FAQ)</router-link></li>
+            </ul>
+            <ul class="divUl py-2 d-none d-md-inline-block flex-fill move-left">
+              <li><router-link to="/">전문상담</router-link></li>
+              <li><router-link to="/BoardList">게시판상담</router-link></li>
+              <li><router-link to="/">지도상담</router-link></li>
+              <li><router-link to="/GroupList">집단상담</router-link></li>
+              <li><router-link to="/counseling">심리상담</router-link></li>
+            </ul>
+            <ul class="divUl py-2 d-none d-md-inline-block flex-fill move-left">
+              <li><router-link to="/">이벤트</router-link></li>
             </ul>
             <ul class="divUl py-2 d-none d-md-inline-block flex-fill">
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
-            </ul>
-            <ul class="divUl py-2 d-none d-md-inline-block flex-fill">
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
-            </ul>
-            <ul class="divUl py-2 d-none d-md-inline-block flex-fill">
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
-              <li>상담</li>
+              <li><router-link to="/">카드뉴스</router-link></li>
+              <li><router-link to="/">성장통</router-link></li>
             </ul>
           </nav>
-          <!-- <ul class="divUl">
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-          </ul>
-          <ul class="divUl">
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-          </ul>
-          <ul class="divUl">
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-          </ul>
-          <ul class="divUl">
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-            <li>상담</li>
-          </ul> -->
         </div>
       </nav>
     </div>
@@ -229,19 +195,30 @@ a:active {
   color : black;
 }
 
+.nav-menu .move-left{
+  margin-left:-65px;
+}
+
 .divUl{
   display: flex;
   align-content: flex-start;
   flex-direction: column;
-  flex-wrap: wrap
+  flex-wrap: wrap;
+
+  
+  list-style: none;
+  padding: 0px;
+  margin-left: -30px;
 }
 .divUl li {
-  list-style: none;
   margin-bottom:10px;
-  padding-right: 30px;
-  }
+}
 .parentsDiv{
   display: flex;
 }
 
 </style>
+
+
+
+
