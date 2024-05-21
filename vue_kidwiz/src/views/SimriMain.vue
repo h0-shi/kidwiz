6b1b4e6a1e7da5f15bacbfa43c1ceed427fe2ea4
chorok5 <cr.greenapple@gmail.com>
2024-05-21 12:28:33 +0900

    up

--- vue_kidwiz/src/views/SimriMain.vue
+++ vue_kidwiz/src/views/SimriMain.vue
@@ -1,11 +1,91 @@
 <template>
-    <div>
-      <router-view></router-view>
+  <div>
+    <br>
+    <div class="sbmContainer boundary">
+        <table class="subMenu">
+            <tr>
+                <th :style="select[0]" @click="displayAct(0)">심리검사 안내</th>
+                <th :style="select[1]" @click="displayAct(1)">심리 상담사 소개</th>
+                <th :style="select[2]" @click="displayAct(2)">센터 오시는 길</th>
+                <th :style="select[3]" @click="displayAct(3)">버444튼4</th>
+            </tr>
+        </table>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "SimriMain",
-  };
-  </script>
+    <section class="section s1" :style="cssAct[0]">
+        <h1>심리검사 안내</h1>
+    </section>
+    <section class="section s2" :style="cssAct[1]">
+      심리 상담사 소개
+    </section>
+    <section class="section s3" :style="cssAct[2]">
+      센터 오시는 길
+    </section>
+    <section class="section s4" :style="cssAct[3]">
+        테스트4
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            act : "none",
+            cssAct: [
+                {display : "none"},
+                {display : "none"},
+                {display : "none"},
+                {display : "none"}
+            ],
+            select: [
+                {},
+                {},
+                {},
+                {}
+            ],
+        }
+    },
+    mounted(){
+        this.cssAct[0] = {display:"block"};
+        this.select[0] = {border:"2px solid rgb(43, 43, 165", 'border-bottom':"none"};
+    },
+    methods:{
+        displayAct(num){
+            this.cssAct = this.cssAct.map((style, i) => {
+            if(i == num){
+                return {display:"block"};                
+            }
+            return {display:"none"};
+        })
+            this.select = this.select.map((style, i)=> {
+                if(i==num){
+                    return {border:"2px solid rgb(43, 43, 165", 'border-bottom':"none"};
+                }
+            })
+        }
+    },
+
+}
+</script>
+
+<style>
+.sbmContainer{
+    width: 100%;
+    margin-bottom: 50px;
+}
+.subMenu{    
+    width: 100%;
+    margin: auto;
+}
+.subMenu tr th{
+    width: 25%;
+    height: 55px;
+    border-bottom: 2px solid rgb(43, 43, 165);
+}
+.boundary{
+    width: 100%;
+    max-width: 1200px;
+    padding: 0 auto;
+}
+
+</style>
