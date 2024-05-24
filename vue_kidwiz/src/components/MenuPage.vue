1341e2fccc29b5677d06c208227e6333d4c512f4
moonee <leenaland@gmail.com>
2024-05-24 09:07:50 +0900

    0524 문희

--- vue_kidwiz/src/components/MenuPage.vue
+++ vue_kidwiz/src/components/MenuPage.vue
@@ -32,9 +32,7 @@
       </nav>
     <div class="secNav boundary" :style="{display: navDisplay}" @mouseover="navAct()" @mouseleave="navDead()">
       <nav class="nav-sec">
-        <ul name="introduce">
-          <li><router-link to="/listTest">DB테스트</router-link></li>
-          <li><router-link to="/insertTest">insert 테스트</router-link></li>          
+        <ul name="introduce">        
           <li><router-link to="/admin">관리자페이지</router-link></li>
           <li> </li>
           <li> </li>
